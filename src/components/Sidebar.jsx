483108b4a9a57cef1aeb3dbbe5f9b3319ee77652
Madhurima30 <ch.madhurimaa@gmail.com>
2022-09-05 10:25:50 -0500

    pages
    commiting all the pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,14 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { links } from "../data/dummy";
 import Button from "./Button";
 import { itemsToOrder } from "@syncfusion/ej2/treemap";
+import { useStateContext } from "../contexts/ContextProvider";
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
@@ -27,7 +33,7 @@ const Sidebar = () => {
           >
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold
       tracking-tight dark:text-white text-slate-900"
             >
@@ -36,7 +42,7 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() => setActiveMenu((prevState) => !prevState)}
                 className="text-xl rounded-full
               p-3 hover:bg-light-gray mt-4 block
               "
@@ -58,7 +64,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSidebar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
